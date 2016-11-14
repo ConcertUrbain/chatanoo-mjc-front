355bb07b9554f3ff36a2fec90138d65873e3f3a7
Mathieu Desvé <mathieu@ogury.co>
2016-11-14 11:42:18 +0100

    Configure Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,15 @@ mkdir build
 zip -r build/application.zip . -x *.git* build\*
 
 echo "Deploy $TRAVIS_TAG version to S3"
-aws s3 cp infra/front.cfn.yml s3://chatanoo-deployment.eu-west-1/infra/front/$TRAVIS_TAG.cfn.yml
-aws s3 cp build/application.zip s3://chatanoo-deployment.eu-west-1/front/$TRAVIS_TAG.zip
+aws s3 cp infra/front.cfn.yml s3://chatanoo-deployments-eu-west-1/infra/front/mjc/$TRAVIS_TAG.cfn.yml
+aws s3 cp build/application.zip s3://chatanoo-deployments-eu-west-1/front/mjc/$TRAVIS_TAG.zip
 
 echo "Upload latest"
 aws s3api put-object \
-  --bucket chatanoo-deployment.eu-west-1 \
-  --key infra/front/latest.cfn.yml \
-  --website-redirect-location /infra/front/$TRAVIS_TAG.cfn.yml
+  --bucket chatanoo-deployments-eu-west-1 \
+  --key infra/front/mjc/latest.cfn.yml \
+  --website-redirect-location /infra/front/mjc/$TRAVIS_TAG.cfn.yml
 aws s3api put-object \
-  --bucket chatanoo-deployment.eu-west-1 \
-  --key front/application/latest.zip \
-  --website-redirect-location /front/front/$TRAVIS_TAG.zip
+  --bucket chatanoo-deployments-eu-west-1 \
+  --key front/mjc/latest.zip \
+  --website-redirect-location /front/mjc/$TRAVIS_TAG.zip
